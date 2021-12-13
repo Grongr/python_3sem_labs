ec34909bcf0e0ac80eda7987ca09db239aee60ee
Egor Osipov <e.a.osipov@mail.ru>
2021-12-13 16:52:12 +0300

    Solve problems eith third task

--- pandas/pandas_lab.py
+++ pandas/pandas_lab.py
@@ -31,10 +31,12 @@ def third_task():
     students_info = pd.read_excel('students_info.xlsx')
     resultes = pd.read_html('results_ejudge.html') 
     resultes = resultes[0]
-    m = students_info.merge(resultes, left_on= 'login', right_on= 'User', how= 'outer')
+    m = students_info.merge(resultes, left_on= 'login',
+                            right_on= 'User', how= 'outer')
     m.dropna(subset = ['login'], inplace= True)
     plt.subplot(1,2,1)
-    m.groupby(by = 'group_faculty')['Score'].mean().plot.bar(rot= 0, color= 'red' )
+    k = m.groupby(by = 'group_faculty')['Solved'].mean()
+    k.plot.bar(rot= 0, color='red')
     plt.subplot(1,2,2)
     m.groupby(by = 'group_out')['Score'].mean().plot.bar(rot= 0)
     f = m.copy()
@@ -42,7 +44,8 @@ def third_task():
     z.dropna(subset= ['G'], inplace= True)
     f.dropna(subset= ['H'], inplace= True)
     cool_guys = pd.concat((z,f), axis = 0)
-    print(cool_guys['group_faculty'].value_counts(), '\n',
+    print(cool_guys['group_faculty'].value_counts(),
+          '\n------------------------------------\n',
           cool_guys['group_out'].value_counts())
     plt.show()
 
@@ -52,10 +55,10 @@ if __name__ == "__main__":
     print("------------------------Firts---------------------")
     first_task()
 
-    # print("##################################################")
-    # print("----------------------Second----------------------")
-    # second_task()
+    print("##################################################")
+    print("----------------------Second----------------------")
+    second_task()
 
-    # print("##################################################")
-    # print("-----------------------Third----------------------")
-    # third_task()
+    print("##################################################")
+    print("-----------------------Third----------------------")
+    third_task()
